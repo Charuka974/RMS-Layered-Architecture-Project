a8ceb11a04430f4097c0ee10b9c882cb55154ee2
Charuka974 <charuka974@gmail.com>
2025-02-05 01:32:17 +0530

    BOFactory and DAOFactory complete

--- src/main/java/org/gourmetDelight/dao/SQLUtil.java
+++ src/main/java/org/gourmetDelight/dao/SQLUtil.java
@@ -1,4 +1,24 @@
-package org.gourmetDelight.dao;
+package org.gourmetDelight.util;
 
-public class SQLUtil {
+import org.gourmetDelight.db.DBConnection;
+
+import java.sql.*;
+
+public class CrudUtil{
+    public static <T>T execute(String sql, Object... obj) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement pst = connection.prepareStatement(sql);
+        for(int i = 0 ; i < obj.length; i++){
+            pst.setObject((i+1), obj[i]);
+        }
+        if (sql.startsWith("SELECT") | sql.startsWith("select")) {
+            ResultSet resultSet = pst.executeQuery();
+            return (T) resultSet;
+        }
+        else{
+            int i = pst.executeUpdate();
+            boolean isDone = i > 0;
+            return (T) ((Boolean)isDone);
+        }
+    }
 }
